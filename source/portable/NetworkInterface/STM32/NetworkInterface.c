b73113fc6cee521541862163f15d41e62e0dc7ba
Phil Greenland <phil@quantulum.co.uk>
2026-02-20 19:04:54 +0000

    Enable reception of broadcast packets for ARP

--- source/portable/NetworkInterface/STM32/NetworkInterface.c
+++ source/portable/NetworkInterface/STM32/NetworkInterface.c
@@ -1117,7 +1117,7 @@ static void prvInitMacAddresses( ETH_HandleTypeDef * pxEthHandle,
     xFilterConfig.SrcAddrFiltering = DISABLE;
     xFilterConfig.SrcAddrInverseFiltering = DISABLE;
     xFilterConfig.ControlPacketsFilter = ETH_CTRLPACKETS_BLOCK_ALL;
-    xFilterConfig.BroadcastFilter = ENABLE;
+    xFilterConfig.BroadcastFilter = DISABLE; // PG: Was filtering out broadcast packets, which are required for ARP
     xFilterConfig.PassAllMulticast = DISABLE;
     xFilterConfig.DestAddrInverseFiltering = DISABLE;
     xFilterConfig.HashMulticast = ENABLE;
